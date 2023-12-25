6a9b0234da0a9f664893e0f64e914894ede93917
Wail <wailabualela@gmail.com>
2023-12-25 11:47:17 +0300

    Add my cv

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import "./About.css";
 
 import AboutImg from "../../assets/me.jpg";
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/Resume-Wail-Abualela.pdf";
 import Info from "./Info";
 import React from "react";
 
